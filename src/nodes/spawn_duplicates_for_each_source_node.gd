d0c05463c38703af5ee37122fa09f16b345dc6fc
Aleksandar Danilovic <greatgames.alexandar@gmail.com>
2020-05-25 05:01:18 +0200

    save of new spawner node working

--- src/nodes/spawn_duplicates_for_each_source_node.gd
+++ src/nodes/spawn_duplicates_for_each_source_node.gd
@@ -16,22 +16,20 @@ func _init() -> void:
 
 
 func _generate_outputs() -> void:
-	var sources = get_input_single(0)
+	var sources = get_input(0)
 	var transforms := get_input(1)
 
-	print ("spawn_arrays: generating outputs")
 	if not sources or sources.size() == 0 or not transforms or transforms.size() == 0:
 		return
 	
-	print ("spawn_arrays: exists sources ", sources, " size ", sources.size(), " transforms ",transforms, " size ",transforms.size())
 	var s_num = 0
-	var t_num = 0
 	for s in sources:
 		for t in transforms:
+			var t_num = 0
 			var n = s.duplicate() as Spatial
 			#n.global_transform = t.transform
-			n.global_transform.origin = n.global_transform.origin+t.transform.origin
-			print ("duplicating s_num ",s_num, " t_num", t_num, " at ", n.global_transform.origin, " from s ",n.global_transform.origin, " and t ",t.transform.origin)
+			n.transform.origin = s.transform.origin+t.transform.origin
 			
 			output[0].append(n)
-			
+			t_num+=1
+		s_num+=1
